60207a8db0f894eb7112760cd1093fa8c82c3cc3
dragon818 <dragon.yao1219@gmail.com>
2019-12-05 14:27:58 -0600

    make the enemies come out

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const imgmin = require('gulp-imagemin');
 
 function watcher () {
-  watch('./src/images/**/*.png', copyimg)
+  watch('./src/images/**/*', copyimg)
   watch('./src/index.html', copyhtml)
   watch('./src/js/**/*.js', minjs)
   watch('./src/css/**/*.css', style)
@@ -22,9 +22,7 @@ function copyhtml () {
 
 function style () {
   return src('./src/css/**/*.css')
-  .pipe(autoprefixer({
-    browsers:['last 2 versions']
-  }))
+  .pipe(autoprefixer())
   .pipe(cleanCss())
   .pipe(dest('./dest/css'))
   // .pipe(browserSync.stream());
@@ -32,15 +30,15 @@ function style () {
 
 function copyimg () {
   return src('./src/images/**/*')
-  .pipe(imgmin())
+  .pipe(imgmin([imgmin.jpegtran({progressive: true}, imgmin.optipng({optimizationLevel: 5}))]))
   .pipe(dest('./dest/images'))
   // .pipe(browserSync.stream());
 }
 
 function minjs () {
-  return src('./src/js/**/*.js')
+  return src(['./src/js/resources.js' ,'./src/js/app.js' ,'./src/js/engine.js'])
   .pipe(babel({
-    presets: ['@babel/env']
+    presets: [['@babel/env',{modules: false}]]
   }))
   .pipe(concat('main.js'))
   .pipe(uglify())
